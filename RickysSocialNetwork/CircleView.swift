602461af97c572b48268e29f9619b1866fc1439e
ricky04286 <reccles@Richards-MBP.att.net>
2016-12-27 13:10:33 -0600

    Post Function Complete

--- RickysSocialNetwork/CircleView.swift
+++ RickysSocialNetwork/CircleView.swift
@@ -13,6 +13,7 @@ class CircleView: UIImageView {
     override func layoutSubviews() {
         //do the circle config in here becaue the contorller size has now been decided and it will be a more accurate calculation
         layer.cornerRadius = self.frame.width / 2
+        clipsToBounds = true
     }
 
 }
--- RickysSocialNetwork/FeedVC.swift
+++ RickysSocialNetwork/FeedVC.swift
@@ -10,17 +10,23 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAdd: CircleView!
     
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
         
         //initialize the listener
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
@@ -61,8 +67,22 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            imageAdd.image = image
+        }
+        else {
+            print("Ricky: A valid image wasn't selected")
+        }
+        
+        //once you selected an image, get rid of image picker
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
     
-    
+    @IBAction func addImagePressed(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 
     @IBAction func signOutPressed(_ sender: UIButton) {
         //sign out of keychain and firebase
